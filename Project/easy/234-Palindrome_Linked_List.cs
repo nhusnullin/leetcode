5fcd46e822c2d3cff0925749810f4aa5abaf7ba5
Nail Khusnullin <nkhusnullin@swiftcom.uk>
2021-03-19 06:44:13 +0300

    234

---
+++ Project/easy/234-Palindrome_Linked_List.cs
@@ -0,0 +1,71 @@
+using NUnit.Framework;
+
+namespace Project.easy
+{
+    public class Palindrome_Linked_List
+    {
+        [TestCase(new[] {1, 2, 2, 1}, true)]
+        [TestCase(new[] {1, 2, 1}, true)]
+        [TestCase(new[] {1, 2}, false)]
+        public void Test(int[] list, bool result)
+        {
+            Assert.AreEqual(result, IsPalindrome(new ListNode(list)));
+        }
+
+
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+
+            var slow = head;
+            var fast = head;
+            while (fast.next != null && fast.next.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            var reverseHead = slow.next;
+            slow.next = null;
+
+            reverseHead = Reverse(reverseHead);
+
+            while (reverseHead != null && head != null)
+            {
+                if (reverseHead.val != head.val)
+                {
+                    return false;
+                }
+
+                reverseHead = reverseHead.next;
+                head = head.next;
+            }
+
+            return true;
+            
+        }
+
+
+        private ListNode Reverse(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            var currentHead = head;
+            while (head.next != null)
+            {
+                var p = head.next;
+                head.next = p.next;
+                p.next = currentHead;
+                currentHead = p;
+            }
+
+            return currentHead;
+        }
+    }
+}
